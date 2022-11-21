a7800a8e10ef0505eb2b27fb624e46359ab3d54c
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-11-22 02:40:42 +0530

    Continuing with next.

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "./../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeBBg1sczduRclnYTc-kgAPkA0FGZ6rNoyoA&usqp=CAU"
+      title="A First Meetup"
+      address="Some Address, Country"
+      description="This is a first Meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
